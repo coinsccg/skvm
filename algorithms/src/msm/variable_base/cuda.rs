60d6900886ef124134a3989e9af3a6aea225058d
coinsccg <825076979@qq.com>
2022-07-18 14:50:40 +0800

    init

--- algorithms/src/msm/variable_base/cuda.rs
+++ algorithms/src/msm/variable_base/cuda.rs
@@ -568,8 +568,8 @@ fn initialize_cuda_request_handler(input: crossbeam_channel::Receiver<CudaReques
             let mut context = context.clone();
             let cuda_thread1 = cuda_thread.clone();
             cuda_thread.fetch_add(1,  Ordering::SeqCst);
+            eprintln!("--------------------------------------------------------------------------{}", cuda_thread.load(Ordering::SeqCst));
             std::thread::spawn(move || {
-                eprintln!("--------------------------------------------------------------------------{}", len);
                 let out = handle_cuda_request(&mut context, &request, index);
                 request.response.send(out).ok();
                 cuda_thread1.fetch_sub(1,  Ordering::SeqCst);
